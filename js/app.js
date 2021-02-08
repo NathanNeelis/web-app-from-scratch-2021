81b00b0649b9abd9f1c3670e9ff473ff2e806034
NathanNeelis <nathan.neelis@hva.nl>
2021-02-08 15:34:01 +0100

    added search bar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-console.log('hello world, javascript is working')
-
 // example API request -> this is for a specific movie.
-const endpoint = 'https://api.themoviedb.org/3/movie/550?api_key=172dbac1b2ced3673820d2a54c969fe1'; // movie 550 
+// const endpoint = 'https://api.themoviedb.org/3/movie/550?api_key=172dbac1b2ced3673820d2a54c969fe1'; // movie 550 
 
 // image example 
 const movieImage = 'https://image.tmdb.org/t/p' // starter url for images
@@ -9,54 +7,126 @@ const movieImageWidth = '/w500'; // the width of the image is 500
 const movieImageUrl = 'kqjL17yufvn9OVLyXYpvtyrFfak.jpg'; // This is data from the API
 
 // endpoint themoviedb -> top 20 most popular movies
-const url = 'https://api.themoviedb.org/3/discover/movie?'; // discover movies url
+const endpoint = 'https://api.themoviedb.org/3'; // base url
+
+// endpoint variables
+const discover = "/discover"
+const search = "/search"
+const movie = "/movie?"
 const key = 'api_key=172dbac1b2ced3673820d2a54c969fe1'; // api key
 const language = '&language=en-US'; // language
+const adult = '&include_adult=false';
 const sort = '&sort_by=popularity.desc'; // sort by popularity
 const page = '&page=1'; // page 1/500
+const preSearch = '&query=';
+
+// API URLS
+const urlTopMovies = endpoint + discover + movie + key + language + sort + page;
+const urlSearch = endpoint + search + movie + key + language + adult + preSearch;
 
 
-makeContent()
-async function makeContent() {
-    getData(url, key, language, sort, page)
+
+// render page 
+renderTopMovies()
+defaultSearch()
+
+// get input value
+async function defaultSearch() {
+
+    // selection section to render html
+    const section = document.querySelector('.searchResults');
+
+    // storage value
+    let storageValue = localStorage.getItem('searching')
+
+    // checkForStorage(storageValue, section);
+    checkForStorage(storageValue);
+
+    getData(urlSearch + storageValue)
         .then(data => {
 
             // function(s) to transform data  
             let resultsPageOne = data.results;
             let cleanObject = filterObject(resultsPageOne)
-            // let topTwentyTitles = filterData(cleanObject, 'title')
-            // let topTwentyImages = filterData(cleanObject, 'poster_path')
 
-
-            // console logs
-            console.log('Incoming data', data)
-            console.log('results page 1', resultsPageOne)
-            console.log('new object', cleanObject)
-
+            console.log(data)
 
             // function(s) to render data
-            arrayToHtml(cleanObject)
+            setSearchBar(); // Sets local storage item in search bar
+            section.innerHTML = ''; // remove existing HTML
+            renderToHtml(cleanObject, section) // renders the top movies
+        });
+
+}
+
+// get input value
+async function getInput() {
+
+    // saving input in local storage
+    const storage = window.localStorage;
+    let search = document.querySelector('.searchField').value;
+    storage.setItem('searching', search)
+    console.log('this is what you are searching for:', search)
+
+    // selection section to render html
+    const section = document.querySelector('.searchResults');
+
+    // storage value
+    let storageValue = localStorage.getItem('searching')
+
+    checkForStorage(storageValue);
+
+    getData(urlSearch + storageValue)
+        .then(data => {
+
+            // function(s) to transform data  
+            let resultsPageOne = data.results;
+            let cleanObject = filterObject(resultsPageOne)
+
+            console.log(data)
+
+            // function(s) to render data
+            setSearchBar(); // Sets local storage item in search bar
+            section.innerHTML = ''; // remove existing HTML
+            renderToHtml(cleanObject, section) // renders the top movies
+        });
+
+}
+
+const searchButton = document.querySelector('.searchBtn')
+searchButton.addEventListener("click", getInput)
+
+async function renderTopMovies() {
+    getData(urlTopMovies)
+        .then(data => {
+
+            // function(s) to transform data  
+            let resultsPageOne = data.results;
+            let cleanObject = filterObject(resultsPageOne)
+            const section = document.querySelector('.topTwenty');
+
+            // console logs
+            // console.log('Incoming data', data)
+            // console.log('results page 1', resultsPageOne)
+            // console.log('new object', cleanObject)
+
+            // function(s) to render data
+            setSearchBar(); // Sets local storage item in search bar
+            renderToHtml(cleanObject, section) // renders the top movies
 
         });
 }
 
 
-async function getData(url, key, language, sort, page) {
-    const response = await fetch(url + key + language + sort + page);
+async function getData(URL) {
+    const response = await fetch(URL);
     const data = await response.json();
     return data;
 }
 
 
-// returns an array of all data in a specific column
-function filterData(dataArray, column) {
-    return dataArray.map(result => result[column]);
-}
-
-
 // OUTPUT HTML P ELEMENTS IN DIV ELEMENTS
-function arrayToHtml(data) {
-    let section = document.querySelector('.topTwenty'); // selects the dom element where to add the new data
+function renderToHtml(data, section) {
     for (let id in data) {
         // create DIV
         let newElement = document.createElement('div'); // creates a div
@@ -74,7 +144,7 @@ function arrayToHtml(data) {
 
         // Create IMG element
         let movieImages = document.createElement("img");
-        movieImages.src = 'https://image.tmdb.org/t/p/w200' + data[id].poster_path;
+        movieImages.src = 'https://image.tmdb.org/t/p/w200/' + data[id].poster_path;
 
         // append 
         section.appendChild(newElement); // appends the divs to the section
@@ -97,4 +167,24 @@ function filterObject(results) {
     });
 
     return items
+}
+
+// set searchbar with latest search item
+function setSearchBar() {
+    let storageValue = localStorage.getItem('searching')
+    let search = document.querySelector('.searchField');
+    search.value = storageValue;
+}
+
+// checks if there is an item in storage. IF not, hides the search results
+function checkForStorage(storageValue) {
+    const wrapper = document.querySelector('.searchResultsWrapper');
+    wrapper.classList.remove('hide')
+
+    if (storageValue == null) {
+        wrapper.classList.add('hide');
+
+    } else if (storageValue != null) {
+        wrapper.classList.remove('hide');
+    }
 }
